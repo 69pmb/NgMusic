ac39229cb5bd4f35cba7869f369f753fd9c8c71c
pierre-marie broca <pierre-marie.broca@axess.fr>
2019-01-07 15:41:55 +0100

    css -> scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Composition, Fichier } from './utils/model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'app';
