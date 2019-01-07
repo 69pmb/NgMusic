08fd53f124a1e25ec1d844362c763385d0a80dd1
pierre-marie broca <pierre-marie.broca@axess.fr>
2019-01-07 14:10:48 +0100

    first commit: poc reading xml

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { DropboxService } from './services/dropbox.service';
+import { Component, OnInit } from '@angular/core';
+
+import * as xml2js from 'xml2js';
+import { Composition, Fichier } from './utils/model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  compoList: Composition[];
+
+  constructor(
+    private dropboxService: DropboxService
+  ) { }
+
+  ngOnInit() {
+    const parser = new xml2js.Parser();
+    let t0 = performance.now();
+
+    this.dropboxService.downloadFile('AllMusic.xml').then(file => {
+      parser.parseString(file, (err, result) => {
+        // parser.parseString(file, { tagNameProcessors: [this.nameToLowerCase] }, (err, result) => {
+        console.dir(result);
+        console.log('Done');
+        this.compoList = [];
+        result.ListCompositions.compo.forEach(el => {
+          console.log(el);
+          const c = new Composition(el.$.A, el.$.T, el.$.type, el.$.del, el.$.mergeable);
+          const fileList = [];
+          el.file.forEach((elFile: any) => {
+            const fichier = new Fichier(elFile.$.author, elFile.$.cat, elFile.$.creation, elFile.$.name,
+              elFile.$.publish, elFile.$.rangeB, elFile.$.rangeE, elFile.$.rank, elFile.$.size, elFile.$.sorted);
+            fileList.push(fichier);
+          });
+          c.fileList = fileList;
+          this.compoList.push(c);
+        });
+        console.log('compoList', this.compoList);
+        let t1 = performance.now();
+        console.log('Call to doSomething took ' + (t1 - t0) + ' milliseconds');
+      });
+    });
+  }
+
+  nameToLowerCase(name: string) {
+    return name.toLowerCase();
+  }
+
 }
---
+++ src/app/services/dropbox.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import * as Dropbox from 'dropbox';
+
+import { UtilsService } from './utils.service';
+import { Dropbox as DropboxConstante } from '../utils/dropbox';
+
+@Injectable({ providedIn: 'root' })
+export class DropboxService {
+
+  constructor(
+    private serviceUtils: UtilsService
+  ) { }
+
+  getDbx(): Dropbox.Dropbox {
+    return new Dropbox.Dropbox({ accessToken: DropboxConstante.DROPBOX_TOKEN });
+  }
+
+  listFiles(): void {
+    this.getDbx().filesListFolder({ path: '' })
+      .then((response: Dropbox.files.ListFolderResult) => console.log(response.entries))
+      .catch((err) => this.serviceUtils.handleError(err));
+  }
+
+  getPath(fileName: string): string {
+    return DropboxConstante.DROPBOX_FOLDER + fileName;
+  }
+
+  uploadFile(fichier: Blob, fileName: string): Promise<Dropbox.files.FileMetadata> {
+    const pathFile = this.getPath(fileName);
+    return this.getDbx().filesDeleteV2({ path: pathFile })
+      .then((response: Dropbox.files.DeleteResult) => {
+        return this.getDbx().filesUpload({ path: pathFile, contents: fichier });
+      }).catch((err) => this.serviceUtils.handlePromiseError(err));
+  }
+
+  uploadNewFile(fichier: string, fileName: string): Promise<Dropbox.files.FileMetadata> {
+    const pathFile = this.getPath(fileName);
+    return this.getDbx().filesUpload({ path: pathFile, contents: fichier })
+      .then(() => new Promise((resolve, reject) => resolve()))
+      .catch((err) => this.serviceUtils.handlePromiseError(err));
+  }
+
+  downloadFile(fileName: string): Promise<any> {
+    return this.getDbx().filesDownload({ path: this.getPath(fileName) })
+      .then((response: any) => {
+        const fileReader = new FileReader();
+        return new Promise((resolve, reject) => {
+          fileReader.onerror = () => {
+            fileReader.abort();
+            reject(new DOMException('Problem parsing input file.'));
+          };
+          fileReader.onload = () => {
+            return resolve(fileReader.result.toString());
+          };
+          fileReader.readAsText(response.fileBlob);
+        });
+      })
+      .catch((err) => this.serviceUtils.handleError(err));
+  }
+}
