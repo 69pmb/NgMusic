35eb9103921674ef25bf673962ea8a0beb1d88f5
pierre-marie broca <pierre-marie.broca@axess.fr>
2019-01-07 15:38:43 +0100

    add list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
@@ -12,9 +16,13 @@ import { ListComponent } from './list/list.component';
       ListComponent
    ],
    imports: [
+      BrowserAnimationsModule,
       BrowserModule,
       HttpClientModule,
-      HttpClientJsonpModule
+      HttpClientJsonpModule,
+      MatTableModule,
+      MatSortModule,
+      MatPaginatorModule,
    ],
    providers: [],
    bootstrap: [
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { Sort } from '@angular/material/sort';
+
+import { Composition } from '../utils/model';
+import { Utils } from '../utils/utils';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+  compoList: Composition[];
+  displayedColumns = ['artist', 'title', 'type'];
+  length: number;
+  displayedData: Composition[];
+  pageSize = 25;
+  pageIndex = 0;
+  pageSizeOptions = [10, 25, 50, 100];
+  page: PageEvent;
+  sort: Sort;
+
+  constructor(
+    private elemRef: ElementRef,
+  ) { }
+
+  ngOnInit() {
+    this.compoList = JSON.parse(sessionStorage.getItem('compoList'));
+    this.length = this.compoList.length;
+    this.initPagination(this.refreshData());
+  }
+
+  refreshData(): Composition[] {
+    const list = Utils.sortComposition(this.compoList, this.sort);
+    this.length = list.length;
+    return list;
+  }
+
+  initPagination(list: Composition[]): void {
+    if (this.page) {
+      this.page.pageIndex = 0;
+      this.page.pageSize = this.page ? this.page.pageSize : this.pageSize;
+    }
+    this.paginate(list);
+  }
+
+  onSort(): void {
+    this.initPagination(this.refreshData());
+    this.onTop();
+  }
+
+  onPaginateChange(): void {
+    this.paginate(this.refreshData());
+    this.onTop();
+  }
+
+  paginate(data: Composition[]): void {
+    this.displayedData = this.page ?
+      data.slice(this.page.pageIndex * this.page.pageSize, (this.page.pageIndex + 1) * this.page.pageSize) : data.slice(0, this.pageSize);
+  }
+
+  onTop(): void {
+    // this.elemRef.nativeElement.querySelector('.filters').scrollIntoView();
+  }
+
+}
