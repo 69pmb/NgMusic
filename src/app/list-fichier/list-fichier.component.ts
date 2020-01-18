4f80302f148a3274a64f762a503987015ba52285
pmb <pm.broca@gmail.com>
2020-01-18 15:58:24 +0100

    refactor: rename to list-composition & list-fichier

---
+++ src/app/list-fichier/list-fichier.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-fichier',
+  templateUrl: './list-fichier.component.html',
+  styleUrls: ['./list-fichier.component.scss']
+})
+export class ListFichierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
