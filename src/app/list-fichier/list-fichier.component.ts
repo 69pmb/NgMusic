9bee0265801e8dff7e1775cbaa5ad65b42115d69
pmb <pm.broca@gmail.com>
2020-01-19 22:03:37 +0100

    feat: type, author & publish columns + expand compositions

--- src/app/list-fichier/list-fichier.component.ts
+++ src/app/list-fichier/list-fichier.component.ts
@@ -1,25 +1,35 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { BehaviorSubject } from 'rxjs';
+import { skipWhile } from 'rxjs/operators';
+import { Sort } from '@angular/material/sort';
 
 import { ListComponent } from '../list/list.component';
 import { Fichier } from '../utils/model';
 import { DataService } from '../services/data.service';
 import { UtilsService } from '../services/utils.service';
-import { BehaviorSubject } from 'rxjs';
-import { skipWhile } from 'rxjs/operators';
 import { DexieService } from '../services/dexie.service';
+import { Utils } from '../utils/utils';
 
 @Component({
   selector: 'app-list-fichier',
   templateUrl: './list-fichier.component.html',
-  styleUrls: ['./list-fichier.component.scss']
+  styleUrls: ['./list-fichier.component.scss'],
+  animations: [
+    trigger('compositionExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0', display: 'none' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ])
+  ]
 })
 export class ListFichierComponent extends ListComponent<Fichier> implements OnInit {
-  displayedColumns = ['name', 'type', 'category', 'size'];
-  displayedColumnsComposition = ['artist', 'title', 'type', 'sizeC', 'score'];
+  displayedColumns = ['author', 'name', 'type', 'category', 'sizeF', 'publish'];
+  displayedColumnsComposition = ['artist', 'title', 'rank', 'size', 'score'];
   displayedCompositions = new BehaviorSubject([]);
   expandedElement: Fichier;
-  // expandedColumn = 'details';
+  expandedColumn = 'compositions';
   faAngleUp = faAngleUp;
 
   constructor(
@@ -47,8 +57,20 @@ export class ListFichierComponent extends ListComponent<Fichier> implements OnIn
     this.length = list.length;
     return list;
   }
+
   sortList(list: Fichier[]): Fichier[] {
     return list;
+  }
+
+  expand(element: Fichier): void {
+    this.expandedElement = this.expandedElement === element ? undefined : element;
+    if (this.expandedElement) {
+      this.displayedCompositions.next(Utils.sortComposition(this.expandedElement.compoList, { active: 'rank', direction: 'asc' }));
+    }
+  }
+
+  onSortComposition(sort: Sort): void {
+    this.displayedCompositions.next(Utils.sortComposition(this.expandedElement.compoList, sort));
   }
 
   goTop(): void {
