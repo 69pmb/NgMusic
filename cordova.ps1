e9cb9eec2417c826d39b3d40a090c2a3c8e479b1
Pierre-Marie Broca <pierre-marie.broca@atos.net>
2020-01-24 17:38:21 +0100

    chore: java to 1.8 in cordova script

--- cordova.ps1
+++ cordova.ps1
@@ -1,10 +1,17 @@
+# Set Java to 1.8
+Set-Variable -Name JAVA_HOME -Value "C:\Javatools\java\jdk1.8.0_11\bin;"
+Set-Item -path env:path -force -value ($JAVA_HOME + $env:path) 
+java -version
+
+# Variables
 $dir = Get-Location
-Set-Variable -Name "workspace" -Value "C:\DEV\workspace\NgMusic"
+Set-Variable -Name "workspace" -Value "C:\workspace\NgMusic"
 $apkDir = $workspace + "\cordova\NgMusic\platforms\android\app\build\outputs\apk\debug"
-Set-Variable -Name "outputDir" -Value "C:\Users\PBR\Dropbox\Documents\Dev Perso"
+Set-Variable -Name "outputDir" -Value "C:\Utilisateurs\a756122\Downloads"
 
 cd $workspace
 
+# build app, move it in /cordova and build apk
 yarn cordova
 rm -r -fo cordova\NgMusic\www
 mkdir cordova\NgMusic\www
@@ -12,10 +19,12 @@ xcopy /s dist cordova\NgMusic\www
 cd cordova\NgMusic
 cordova build android
 
+# Rename and move apk
 $newName = "NgMusic_" + (Get-Date -UFormat '%Y.%m.%dT%H.%M.%S') + ".apk"
 Rename-Item -Path ($apkDir + "\app-debug.apk") -NewName $newName
 Move-Item ($apkDir + "\" + $newName) -Destination $outputDir -force
 
+# Test result
 if((Get-Item ($outputDir + "\" + $newName)).length -lt 300KB) {
 	Write-Host "AN ERROR OCCURRED" -ForegroundColor Red
 } else {
